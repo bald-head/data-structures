5b3512532e041b5177a971458d1baf4edf17a8f7
邓天祥 <tianxiang_deng@sina.com>
2020-08-22 22:01:29 +0800

    添加了稀疏数组存入磁盘以及从磁盘中读取稀疏数组并还原二维数组的内容

---
+++ src/com/ytfs/sparsearray/ReadAndSaveSparseArrayToFile.java
@@ -0,0 +1,110 @@
+package com.ytfs.sparsearray;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author by ytfs
+ * @Description 用一句话描述当前类的作用
+ * @Create 2020/8/22-20:37
+ */
+public class ReadAndSaveSparseArrayToFile{
+
+    /**
+     * 将稀疏数组存入磁盘
+     * @param sparseArray
+     * @return
+     */
+    public static boolean saveSparseArrayToFile(int[][] sparseArray){
+        FileWriter fileWriter = null;
+        //result代表是否存入磁盘成功
+        boolean result = false;
+        File file = new File("F://map.data");
+        //文件不存在就创建一个文件
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            //打开文件写入
+            fileWriter = new FileWriter(file);
+            //遍历稀疏数组
+            for (int[] ints : sparseArray) {
+                fileWriter.write(ints[0] + "\t" + ints[1] + "\t" + ints[2]);
+                fileWriter.write("\r");
+            }
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                //关闭文件写入流
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 从磁盘读取稀疏数组
+     * @return
+     */
+    public static int[][] readSparseArrayToFile(){
+        int[][] sparseArray = null;
+        //readOneRow用于判断是否为第一行
+        boolean readOneRow = false;
+        //文件地址
+        File file = new File("F:\\map.data");
+        BufferedReader bufferedReader = null;
+        try {
+            //读取文件map.data
+            bufferedReader = new BufferedReader(new FileReader(file));
+            //获取文件中的每一行,并且每一行不为空
+            String line = null;
+            int count = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tempStr = line.split("\t");
+                if (!readOneRow){
+                    //当前为第一行,那么就创建一个稀疏数组,稀疏数组的行数 = 有效值个数 + 1
+                    sparseArray = new int[Integer.parseInt(tempStr[2]) + 1][3];
+                    sparseArray[count][0] = Integer.parseInt(tempStr[0]);
+                    sparseArray[count][1] = Integer.parseInt(tempStr[1]);
+                    sparseArray[count][2] = Integer.parseInt(tempStr[2]);
+                    count++ ;
+                    readOneRow = true;
+                }else{
+                    //当前不是文件中的第一行,就直接给稀疏数组赋值
+                    sparseArray[count][0] = Integer.parseInt(tempStr[0]);
+                    sparseArray[count][1] = Integer.parseInt(tempStr[1]);
+                    sparseArray[count][2] = Integer.parseInt(tempStr[2]);
+                    count++;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("还原的稀疏数组：");
+        for (int i = 0;  i < sparseArray.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", sparseArray[i][0], sparseArray[i][1], sparseArray[i][2]);
+        }
+        return sparseArray;
+    }
+}
---
+++ src/com/ytfs/sparsearray/SparseArrayToFile.java
@@ -0,0 +1,95 @@
+package com.ytfs.sparsearray;
+
+
+/**
+ * @author by ytfs
+ * @Description 二维数组和稀疏数组的互相转换
+ * @Create 2020/8/22-16:39
+ */
+public class SparseArrayToFile {
+
+    public static void main(String[] args) {
+        //创建一个二维数组
+        int chessArr[][] = new int[11][11];
+        //给二维数组赋有效数据
+        chessArr[1][2] = 1;
+        chessArr[2][3] = 2;
+        System.out.println("原始的二维数组为：");
+        //遍历出，二维数组的每一行
+        for (int[] row : chessArr) {
+            //遍历的到每一行的数据
+            for (int data : row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        //统计原始二维数组中的有效数据的个数,sum代表二维数组中有效数据的个数
+        int sum = 0;
+        for (int i = 0; i < chessArr.length; i++) {
+            for (int j = 0; j < chessArr.length; j++) {
+                if (chessArr[i][j] != 0){
+                    sum++;
+                }
+            }
+        }
+        System.out.println("有效数据的个数为：" + sum);
+
+        /**
+         * 将二维数组转换为稀疏数组
+         */
+            //1. 创建稀疏数组
+        int sparseArr[][] = new int[sum + 1][3];
+
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+        //2. 给稀疏数组赋值,count用于计数，计算该稀疏数组的第几行
+        int count = 0;
+        for (int i = 0; i < chessArr.length; i++) {
+            for (int j = 0; j < chessArr.length; j++) {
+                if (chessArr[i][j] != 0){
+                    count++;
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr[i][j];
+                }
+            }
+        }
+        System.out.println("当前的稀疏数组：");
+       for (int i = 0;  i < sparseArr.length; i++) {
+                System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1], sparseArr[i][2]);
+        }
+
+        //将稀疏数组存入文件系统
+        boolean result = ReadAndSaveSparseArrayToFile.saveSparseArrayToFile(sparseArr);
+        //存入成功之后才读取文件还原二维数组
+        if (result){
+            /**
+             * 将稀疏数组转换为二维数组
+             */
+            int[][] sparseArrayToFile = ReadAndSaveSparseArrayToFile.readSparseArrayToFile();
+            System.out.println("还原后的二维数组如下：");
+            int reChessArr[][] = new int[sparseArrayToFile[0][0]][sparseArrayToFile[0][1]];
+            for (int[] row : reChessArr) {
+                for (int data : row) {
+                    System.out.printf("%d\t", data);
+                }
+                System.out.println();
+            }
+
+            //给还原后的二维数组赋值
+            for (int i = 1; i < sparseArr.length; i++) {
+                reChessArr[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+            }
+
+            System.out.println("还原并赋值后的二维数组如下：");
+            for (int[] row : reChessArr) {
+                for (int data : row) {
+                    System.out.printf("%d\t", data);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
